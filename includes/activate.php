73107a08c5549aa7b20cc99d2d188df4657ee1bc
birajgtm <birajgtm@gmail.com>
2018-09-30 18:31:35 +0545

    Solved inserted message placing and removed bug code

--- includes/activate.php
+++ includes/activate.php
@@ -1,37 +1,19 @@
 <?php
-
 function r_activate_plugin(){
     //4.7<4.5 = false
     if(version_compare( get_bloginfo('version'), '4.5', '<' )){
     wp_die( __( 'You must update WordPress to use this plugin', 'recipe'));
     }
-
-//global $wpdb;
-//
-//$createSQL              =   "
-//	CREATE TABLE `" . $wpdb->prefix . "techstack_list` (
-//		`ID` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-//		`name` VARCHAR(32) NOT NULL,
-//		PRIMARY KEY (`ID`)
-//	) ENGINE=InnoDB " . $wpdb->get_charset_collate() . " AUTO_INCREMENT=1;";
-//
-//	require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
-//	dbDelta( $createSQL );
-//    
-//}
-
 global $wpdb;
 $table_name = $wpdb->prefix . "techstack_list";
 $charset_collate_is = $wpdb->get_charset_collate();
-
 $createSQL              =   "
 	CREATE TABLE $table_name (
     `ID` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(32) NOT NULL,
     PRIMARY KEY (`ID`)
 	) ENGINE=InnoDB " . $charset_collate_is . " AUTO_INCREMENT=1;";
-
 	require_once( ABSPATH . '/wp-admin/includes/upgrade.php' );
 	dbDelta( $createSQL );
-    
 }
+?>
--- table-view.php
+++ table-view.php
@@ -7,26 +7,17 @@
 * Author URI: birajgtm.com.np
 * Text Domain: table-view
 */
-
 if( !function_exists('add_action') ) {
     die('Hi there, im just a plugin not much i can do when called directly');
 }
-
-//setup
-
-
-
 //includes
 include('includes/activate.php');
-
 //hooks
 register_activation_hook(__FILE__, 'r_activate_plugin');
-
 //shortcodes
 add_shortcode('display', 'custom_form');
 add_shortcode('custom-table', 'fetch_table_data');
-
-//funnctions
+//functions
 function custom_form() {
  ?>
  <form action ="#" method ="post">
@@ -35,43 +26,35 @@ function custom_form() {
  <input type = "submit" name = "submit" value = "Insert">
 </form>
  <?php
-}
 
 if($_POST['submit']) {
  global $wpdb;
 $table_name = $wpdb->prefix . "techstack_list";
 $charset_collate_is = $wpdb->get_charset_collate();
  $name = $_POST['entry'];
- 
- $success = $wpdb->insert($table_name, array(
-   "name" => $name,
-));
- if($success) {
- echo ' Inserted successfully';
-      } else {
-   echo 'not';
+  $success = $wpdb->insert($table_name, array(
+   "name" => $name));
  }
-}
-
-
+    if($success) {
+ echo ' Inserted Successfully';
+      } else {
+   echo 'Not Inserted';}
+ }
 function fetch_table_data(){
 global $wpdb;
 $table_name = $wpdb->prefix . "techstack_list";
 $retrieve_data = $wpdb->get_results( "SELECT * FROM $table_name" );
-    echo'<table>';
+     echo'<table>';
      echo '<tr>';
       echo '<th>ID</th>';
       echo '<th>Name</th>';
       echo '</tr>';
 foreach ( $retrieve_data as $print )   {
-
       echo '<tr>';
       echo '<td>' . $print->ID.'</td>';
       echo '<td>' . $print->name.'</td>';
       echo '</tr>';
   }
     echo'</table>'; 
-    
-}
-
+    }
 ?>
